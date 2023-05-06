44cb19d21a9845b917d829077b2b0bf1af4ba42a
bortek1993 <b.wojciechowski001@gmail.com>
2023-05-06 10:31:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParityOFNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do sprawdzenia czy jest parzysta:");
+            var number = GetNumber();
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("\nLICZBA PARZYSTA");
+            }
+            else
+            {
+                Console.WriteLine("\nLICZBA NIEPARZYSTA");
+            }
+
+        }
+
+        private static int GetNumber()
+        {
+           while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(),out int number))
+                {
+                    Console.WriteLine("Podano nieprawidłową wartość, spróbuj ponownie");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
