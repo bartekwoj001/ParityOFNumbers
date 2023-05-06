b0fb2639c68009183579d50ce3eace18b15d62c8
bortek1993 <b.wojciechowski001@gmail.com>
2023-05-06 11:29:46 +0200

    Start again with numbers to check

--- Program.cs
+++ Program.cs
@@ -10,16 +10,22 @@ namespace ParityOFNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj liczbę do sprawdzenia czy jest parzysta:");
-            var number = GetNumber();
+            Console.WriteLine("Podaj liczbę do sprawdzenia czy jest parzysta. Żeby zakończyć wybierz 't'");
 
-            if (number % 2 == 0)
+            while (true)
             {
-                Console.WriteLine("\nLICZBA PARZYSTA");
-            }
-            else
-            {
-                Console.WriteLine("\nLICZBA NIEPARZYSTA");
+                
+                var number = GetNumber();
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("\nLICZBA PARZYSTA");
+                }
+                else
+                {
+                    Console.WriteLine("\nLICZBA NIEPARZYSTA");
+                }
+                Console.WriteLine("Podaj kolejną liczbę. Żeby zakończyć wybierz 't'");
             }
 
         }
@@ -28,7 +34,12 @@ namespace ParityOFNumbers
         {
            while (true)
             {
-                if (!int.TryParse(Console.ReadLine(),out int number))
+                var value = Console.ReadLine();
+                if(value.ToUpper() == "T")
+                    Environment.Exit(0);
+
+
+                if (!int.TryParse(value, out int number))
                 {
                     Console.WriteLine("Podano nieprawidłową wartość, spróbuj ponownie");
                     continue;
